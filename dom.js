63ab52ef973ab75adb711f5692631e2832ee2e9a
gemgupta <gemgupta007@gmail.com>
2023-06-24 17:21:06 +0530

    123

---
+++ dom.js
@@ -0,0 +1,8 @@
+const mainHead= document.getElementById("header-title");
+ mainHead.textContent= "Hello There"; 
+mainHead.style.background= "blue";
+const Head= document.getElementById("main-header");
+Head.style.border= "solid 5px black "
+const Title= document.getElementsByClassName("title");
+Title[0].style.fontWeight = "bold";
+Title[0].style.color= "green";
