5245b39ca64d456b20c532bb1ba58fe9b0085876
gemgupta <gemgupta007@gmail.com>
2023-06-24 17:12:27 +0530

    gg

---
+++ add.js
@@ -0,0 +1,10 @@
+let ame = 'Yash Prasad'
+
+let age = 25
+
+let address = "Prestige Falcon City, Bangalore"
+
+//console.log(`${personal details = My ame is ${ame} age is ${age} address is ${address}`)
+
+//console.log("personal details = My name is " + ame + "age is" + age + " address is" + address)
+"personal details = My name is ",ame, "age is", age," address is", address
